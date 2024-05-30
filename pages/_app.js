b7460875b770a9172c61a92ee07d70030486c52e
Naveenkumar <knkumar93@users.noreply.github.com>
2024-05-30 13:44:51 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+import { GoogleAdSense } from "nextjs-google-adsense"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <span className="theme-bejamas" />
+      <GoogleAdSense publisherId="ca-pub-4284151384892090" />
       <Component {...pageProps} />
     </>
   );

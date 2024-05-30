d97993afcef62edb844a84d1b95f1da3b1fb0bc4
Naveenkumar <knkumar93@users.noreply.github.com>
2024-05-30 14:04:30 +0530

    Update _app.js for Google AdSense

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <span className="theme-bejamas" />
-      
-      <Script 
-        id="Adsense-id" 
-          data-ad-client="ca-pub-4284151384892090"
-            async strategy="afterInteractive"
-              onError={ (e) => { console.error('Script failed to load', e) }}
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4284151384892090"
-                  />
+      <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4284151384892090" crossorigin="anonymous"/>
       <Component {...pageProps} />
     </>
   );

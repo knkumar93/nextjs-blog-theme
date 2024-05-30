41c335568b6dfaa994929b5188b4b2f1152c8ef2
Naveenkumar <knkumar93@users.noreply.github.com>
2024-05-30 13:55:19 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
-import { GoogleAdSense } from "nextjs-google-adsense"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <span className="theme-bejamas" />
-      <GoogleAdSense publisherId="ca-pub-4284151384892090" />
+      
+      <Script 
+        id="Adsense-id" 
+          data-ad-client="ca-pub-4284151384892090"
+            async strategy="afterInteractive"
+              onError={ (e) => { console.error('Script failed to load', e) }}
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4284151384892090"
+                  />
       <Component {...pageProps} />
     </>
   );

42739ffc29173b52874bcbbc5534687d8dc55187
Naveenkumar <knkumar93@users.noreply.github.com>
2024-05-30 14:07:42 +0530

    Update _app.js for Google AdSense

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,9 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4284151384892090"
+     crossorigin="anonymous"></script>
       <span className="theme-bejamas" />
-      <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4284151384892090" crossorigin="anonymous"/>
       <Component {...pageProps} />
     </>
   );
